109bb2e5e0e52f6f36d6e392ee8545dbec2a947d
LucieO <luciecamilleory@gmail.com>
2020-03-09 13:41:25 +0100

    Updates

---
+++ precache-manifest.2320db92a15e522ba197f7e3149bd2bd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "26455c3f687db4ce6938",
+    "url": "/travel-tips/static/css/main.97aa886a.chunk.css"
+  },
+  {
+    "revision": "26455c3f687db4ce6938",
+    "url": "/travel-tips/static/js/main.26455c3f.chunk.js"
+  },
+  {
+    "revision": "f6dc2c920c42408592c4",
+    "url": "/travel-tips/static/css/1.e68eb2a3.chunk.css"
+  },
+  {
+    "revision": "f6dc2c920c42408592c4",
+    "url": "/travel-tips/static/js/1.f6dc2c92.chunk.js"
+  },
+  {
+    "revision": "8a93018a6a7e1549a3ab",
+    "url": "/travel-tips/static/js/runtime~main.8a93018a.js"
+  },
+  {
+    "revision": "01f1b931c71edbc7592eca9fd5a641b9",
+    "url": "/travel-tips/static/media/travel.01f1b931.png"
+  },
+  {
+    "revision": "c1df6d7016a67f109fa00be222c8196c",
+    "url": "/travel-tips/static/media/compass.c1df6d70.svg"
+  },
+  {
+    "revision": "42f2ed682f53e20ce546763314223fcd",
+    "url": "/travel-tips/static/media/map.42f2ed68.svg"
+  },
+  {
+    "revision": "ae021c74095fa0112a652008c773a543",
+    "url": "/travel-tips/static/media/open-menu.ae021c74.svg"
+  },
+  {
+    "revision": "9bb03c6d1044c25a8390f11d5312f1c6",
+    "url": "/travel-tips/static/media/backpack.9bb03c6d.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/travel-tips/static/media/delete.650cdc57.svg"
+  },
+  {
+    "revision": "73d0609a4b976a0015b47ace4fac1e8e",
+    "url": "/travel-tips/static/media/see.73d0609a.svg"
+  },
+  {
+    "revision": "6cda1fcfdd3fc4a8c4554fca36f798d6",
+    "url": "/travel-tips/static/media/edit.6cda1fcf.svg"
+  },
+  {
+    "revision": "7dc6e5d53d4ab8beede455bf624080c5",
+    "url": "/travel-tips/static/media/star.7dc6e5d5.svg"
+  },
+  {
+    "revision": "2a48b35ba2f6a74dcbf3c9e00eb9bd1a",
+    "url": "/travel-tips/index.html"
+  }
+];
